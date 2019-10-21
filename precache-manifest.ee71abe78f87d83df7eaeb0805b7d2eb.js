4a5b478a336a38968c5d7019f7cf37eda3775535
victor.magarlamov <victor.magarlamov@gmail.com>
2019-10-21 11:18:20 +0300

    Updates

---
+++ precache-manifest.ee71abe78f87d83df7eaeb0805b7d2eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5cbeb90e91e01b20d0f12f5276caea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59bfd66bdb70e9c22e67",
+    "url": "/static/css/main.e256be1d.chunk.css"
+  },
+  {
+    "revision": "d4d518a117b6d8a2dfe9",
+    "url": "/static/js/2.02b958c9.chunk.js"
+  },
+  {
+    "revision": "59bfd66bdb70e9c22e67",
+    "url": "/static/js/main.191e2095.chunk.js"
+  },
+  {
+    "revision": "8418b94035e8d5cfdde4",
+    "url": "/static/js/runtime~main.bc8c70db.js"
+  }
+]);
